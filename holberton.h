012dce83e0be46caec998cc1eeb2d4149d98ed6d
jozsa <allisonjoyweiner@gmail.com>
2018-10-20 22:53:02 +0000

    Added ability to print strings

--- holberton.h
+++ holberton.h
@@ -1,6 +1,15 @@
-#ifndef MYPRINTF_H
-#define PRINT_H
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
+
+/**
+ * specifier - Struct for data types and function pointers
+ * @spec: The data type to be specified by _printf
+ * @f: The corresponding function pointer for each data type
+ *
+ * Description: Data types specified by _printf have corresponding functions
+ */
+
 typedef struct specifier
 {
 	char *spec;
