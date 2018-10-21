5e32ba60640d661190cf28ebf5a056755600a651
jozsa <allisonjoyweiner@gmail.com>
2018-10-21 21:00:44 +0000

    Changed all functions to return type int

--- print_functions.c
+++ print_functions.c
@@ -11,35 +11,40 @@ void _putchar(char c)
 {
 	write(1, &c, 1);
 }
-/**
-**
- * print_int - Prints an integer to standard output
 
-void print_int(va_list args)
+/**
+ * print_int - Prints an integer to standard output
+ *
+
+int print_int(va_list args)
 {
 	int length, index;
-        int *number = va_arg(args, int*);
+	int number;
+
+	number = va_arg(args, int);
 
 	for (length = 1; number[length] != '\0'; length++)
 		;
 	for (index = 0; index < length; index++)
 		_putchar(number[index] + '0');
+	return (length);
 }
 
 **
  * print_char - Prints a character to standard output
  */
 
-void print_char(va_list args)
+int print_char(va_list args)
 {
 	_putchar(va_arg(args, int));
+	return (0);
 }
 
 /**
  * print_str - Prints to the standard output a string passed as an argument to the function
  */
 
-void print_str(va_list args)
+int print_str(va_list args)
 {
 	int length, index;
 	char *str;
@@ -50,14 +55,16 @@ void print_str(va_list args)
 		;
 	for (index = 0;index <length;index++)
 		_putchar(str[index]);
+	return (length);
 }
 
 /**
  * print_per - Prints to the standard output a percent sign passed as an argument to the function
 
 
-void print_per(va_list args)
+int print_per(va_list args)
 {
 	_putchar('\%');
+	return (0);
 }
 */
