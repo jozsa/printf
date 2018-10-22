51bbab95c572074c0d292aa75dd9ffe494f36e92
truong21 <543@holbertonschool.com>
2018-10-22 01:46:52 +0000

    betty style fixes for struct specifier

--- holberton.h
+++ holberton.h
@@ -3,13 +3,12 @@
 #include <stdarg.h>
 
 /**
- * specifier - Struct for data types and function pointers
+ * struct specifier - Struct for data types and function pointers
  * @spec: The data type to be specified by _printf
  * @f: The corresponding function pointer for each data type
  *
  * Description: Data types specified by _printf have corresponding functions
  */
-
 typedef struct specifier
 {
 	char *spec;
